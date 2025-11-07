a51cf4bcbabbfc9072336961491fda83058d9a78
dleiva98 <159450678+dleiva98@users.noreply.github.com>
2025-11-07 00:32:58 -0600

    Update database.js

--- Backend/database.js
+++ Backend/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: '127.0.0.1',
-  port: 3306,
+  port: 3307,
   user: 'root',
   password: 'contrasena',
   database: 'directorio_salud',
